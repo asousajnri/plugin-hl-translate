1589d3206a1f2cb301948c8e3d0875e4562ca518
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-06-22 12:10:48 -0300

    Criando script para tradução

---
+++ content.js
@@ -0,0 +1,26 @@
+(async () => {
+  const translate = (target) => async (word) => {
+    const key = "AIzaSyDCBDC0XEnIJ4Pjf5tjp0J0MdweYzV8NgI";
+
+    const response = await fetch(
+      `https://translation.googleapis.com/language/translate/v2/?key=${key}`,
+      {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          q: [word],
+          target: target,
+        }),
+      }
+    );
+
+    const { data } = await response.json();
+
+    return {
+      sourceLanguage: data.translations[0].detectedSourceLanguage,
+      text: data.translations[0].translatedText,
+    };
+  };
+
+  const translatePortuguese = translate("pt");
+})();
